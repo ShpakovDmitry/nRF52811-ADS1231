b5746fb4b99baf58c32d3abbfc66e9b4f8ad00fd
DmitryShpakov <work.dmitry.shpakov@gmail.com>
2020-10-28 20:33:02 +0200

    ads1231: update

--- ads1231_misc.c
+++ ads1231_misc.c
@@ -3,6 +3,7 @@
 
 static const uint32_t T_STANDBY_MS = 100;
 static const uint32_t T_DATA_READY_MS = 402;
+static const uint8_t DATA_BITS = 24;
 
 void initBus(void) {
     setSckAsOutput();
@@ -21,6 +22,15 @@ void leaveStandByMode(void) {
 }
 
 uint32_t getBits(void) {
+    uint32_t tmp = 0x00000000;
+    
+    setSckLow();
+    for (uint8_t i = 0; i < DATA_BITS; ++i) {
+        setSckHigh();
+        uint8_t dataBit;
+        dataBit = getDataOut();
+
+    }
     //TODO add implementation
 }
 
@@ -36,7 +46,7 @@ void setSckLow(void) {
     //TODO add implementation
 }
 
-uint8_t getDout(void) {
+uint8_t getDataOut(void) {
     //TODO add implementation
 }
 
@@ -44,4 +54,10 @@ void delay_ms(uint32_t ms) {
     //TODO add implementation
 }
 
+void setSckAsOutput(void) {
+    //TODO add implementation
+}
 
+void setDataOutAsInput(void) {
+    //TODO add implementation
+}
--- ads1231_misc.h
+++ ads1231_misc.h
@@ -8,7 +8,9 @@ uint32_t getBits(void);
 int32_t convertTwosToOnesComplement(uint32_t data);
 void setSckHigh(void);
 void setSckLow(void);
-uint8_t getDout(void);
+uint8_t getDataOut(void);
 void delay_ms(uint32_t ms);
+void setSckAsOutput(void);
+void setDataOutAsInput(void);
 
 #endif  // ADS1231_MISC_H
