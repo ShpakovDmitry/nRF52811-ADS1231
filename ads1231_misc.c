5a73c4b6e4e8e21cceca16041812cebe2d3bdab6
DmitryShpakov <work.dmitry.shpakov@gmail.com>
2020-10-28 21:11:50 +0200

    ads1231: update

--- ads1231_misc.c
+++ ads1231_misc.c
@@ -5,6 +5,26 @@
 static const uint32_t T_STANDBY_MS = 100;
 static const uint32_t T_DATA_READY_MS = 402;
 static const uint8_t DATA_BITS = 24;
+static const uint32_t GPIO_PORT_0_BASE_ADDRESS 0x50000000u
+static const uint8_t DOUT_PIN = 11;
+static const uint8_t SCK_PIN = 12;
+
+typedef volatile struct __attribute__((packed)) {
+    uint32_t reserved0[321];    /* 0x000 - 0x500 reserved */
+    uint32_t OUT;               /* 0x504 Write GPIO port */
+    uint32_t OUTSET;            /* 0x508 Set individual bits in GPIO port */
+    uint32_t OUTCLR;            /* 0x50C Clear individual bit in GPIO port */
+    uint32_t IN;                /* 0x510 Read GPIO port */
+    uint32_t DIR;               /* 0x514 Direction of GPIO port */
+    uint32_t DIRSET;            /* 0x518 DIR set register */
+    uint32_t DIRCLR;            /* 0x51C DIR clear register */
+    uint32_t LATCH;             /* 0x520 Latch register */
+    uint32_t DETECTMODE;        /* 0x524 Mode select */
+    uint32_t reserved1[118];    /* 0x528 - 0x6FC reserved */
+    uint32_t PIN_CNF[32];       /* 0x700 - 0x77C Configuration of GPIO pins*/
+} GPIO_Registers;
+
+static GPIO_Registers* gpio0 = (GPIO_Registers* ) GPIO_PORT_0_BASE_ADDRESS;
 
 void initBus(void) {
     setSckAsOutput();
